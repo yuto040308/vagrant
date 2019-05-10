9b707a3f2d9776a8c5e677ae4ffeaf9db2905209
yuto040308 <yuto.ge6@gmail.com>
2019-05-10 14:22:11 +0900

    [Finish] 4-3

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -10,8 +10,17 @@ class BlogsController < ApplicationController
   end
 
   def create
+  	blog = Blog.new(blog_params)
+  	blog.save
+  	redirect_to blogs_path
   end
 
   def edit
   end
+
+  private
+  def blog_params
+  	params.require(:blog).permit(:title, :category, :body)
+  end
+
 end
