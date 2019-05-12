6d5a7e04bbc953980270794b878ddbc2094a45af
yuto040308 <yuto.ge6@gmail.com>
2019-05-12 12:58:40 +0900

    [Update] 4-8 model making

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'bookers/top'
-  get 'bookers/index'
-  get 'bookers/show'
-  get 'bookers/edit'
+  
+  # resourcesで定義するので、一旦無効化する。
+  # get 'bookers/index'
+  # get 'bookers/show'
+  # get 'bookers/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bookers
+
+  root to: 'bookers#top'
 end
