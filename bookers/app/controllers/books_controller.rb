82c900699c9365d4df2eeed2acbaa76398802c55
yuto040308 <yuto.ge6@gmail.com>
2019-05-13 12:24:13 +0900

    [Update] 8-4 update function maked

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -12,12 +12,20 @@ class BooksController < ApplicationController
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
 
   def create
   	book = Book.new(book_params)
   	book.save
   	flash[:notice] = "Book was successfully created."
+  	redirect_to book_path(book.id)
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	flash[:notice] = "Book was successfully updated."
   	redirect_to book_path(book.id)
   end
 
