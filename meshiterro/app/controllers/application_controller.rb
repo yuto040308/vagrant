81f30cebfc45cfa1e183d025347fdcbd7c59d366
yuto040308 <yuto.ge6@gmail.com>
2019-05-13 14:44:35 +0900

    [Finish] 5-2

--- meshiterro/app/controllers/application_controller.rb
+++ meshiterro/app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters
+		# nameのデータ操作を許可するアクションメゾットを指定
+		# 今回の場合は、ユーザ登録(sign_up)の際、ユーザ名(name)のデータ操作が許可される
+		# ストロングパラメータと同様
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+	end
+
 end
