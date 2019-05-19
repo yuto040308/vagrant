ddebc1df809ff9a8f4014aa71cb101f066506b12
yuto040308 <yuto.ge6@gmail.com>
2019-05-19 14:19:20 +0900

    [Update] 5-9 kankyou maked

---
+++ bookers2/app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+end
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'books/create'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/show'
+  get 'books/index'
+  get 'books/destroy'
+  get 'users/edit'
+  get 'users/index'
+  get 'users/show'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
