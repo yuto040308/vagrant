cf5103fc2ad24615e947ba20832ff8d0cf546fe1
yuto040308 <yuto.ge6@gmail.com>
2019-05-20 19:25:07 +0900

    [Update] 5-9 book-new maked

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -1,5 +1,9 @@
 class BooksController < ApplicationController
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to user_path(current_user.id)
   end
 
   def edit
@@ -17,4 +21,15 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  def new
+    
+  end
+
+  # ストロングパラメータ
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+
 end
--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   	@book = Book.new
+  	#@books = Book.all
+  	@books = @user.book.all
   end
 
   def top
