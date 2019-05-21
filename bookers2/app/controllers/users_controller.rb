d4b07af33459f23a999bae5e8c708209441f0edc
yuto040308 <yuto.ge6@gmail.com>
2019-05-21 13:27:45 +0900

    [Update] 5-9 user edit function maked

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -17,8 +17,16 @@ class UsersController < ApplicationController
   end
 
   def update
+  	@user = User.find(params[:id])
+  	@user.update(user_params)
+  	redirect_to user_path(@user.id)
   end
 
   def about
+  end
+
+  # ストロングパラメータ
+  def user_params
+  	params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
--- bookers2/config/initializers/application_controller_renderer.rb
+++ bookers2/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+# refileのselect_key
+Refile.secret_key = '77a642fc016c64929852ef998dda7556ba948f67b0eb8eb385d4477fd0cc6913b66b85fca41007eb50cc3b36e3e8da32e37176fc9b1ad52c000bdc16e6bb345d'
